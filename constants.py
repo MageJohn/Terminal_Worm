6ff9cc26ef3af461ae1d3ad5bdebbf5f09df8070
MageJohn <magejohnyjtp@gmail.fake>
2013-02-12 17:10:24 +0100

    Reorder constants.py to show what is configurable
    Make it clear what is easily configurable in constants.py
    A few constants can't be safely changed without changing many of the files

--- constants.py
+++ constants.py
@@ -31,6 +31,11 @@ ADJUSTMAP = {
     DOWN:  [ 1,  0]
 }
 
+# NB: For configuration purposes, only things
+#     below here should need to be changed
+#============================================
+
+
 APPLECHR = 'O'
 
 # Game window constants
@@ -48,23 +53,24 @@ SCOREPOS = [0, WINWIDTH - 4]
 # Snake constants
 HEADCHR = '@'
 BODYCHR = 'o'
-INITPOS = [int(WINHEIGHT / 2), int(WINWIDTH / 2)]
+INITPOS = [int(WINHEIGHT / 2), int(WINWIDTH / 2)]  # The middle
 INITLENGTH = 4
 assert (INITPOS[1] - INITLENGTH > 0 and
         INITPOS[1] < WINWIDTH and
         INITPOS[0] > 0 and
-        INITPOS[0] < WINHEIGHT), ('The snake needs to start in the window! '
+        INITPOS[0] < WINHEIGHT), (
+        'The snake needs to start in the window! '
         'Remember, the snake is not in the window if '
         'INITPOS - INITLENGTH goes outside the window')
 
-# Intial speed, in seconds
+# Intial speed, in milliseconds
 INITSPEED = 150
 
 WALL_KILL = False  # Set to True if you want running into walls to mean
                    # Game Over.
 
 CONTROL_KEYS = {LEFT: 'h', DOWN: 'j', UP: 'k', RIGHT: 'l'}
-# While the arrow keys will always be switched on, you can set up an
+# While the arrow keys will always be available, you can set up an
 # additional set of keys to control the snake.
 # The default alternative is "HJKL", but if you want to use something
 # else, such as "WASD", or your keyboard layout is'nt QWERTY,
